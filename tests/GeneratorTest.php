d89962179a7b0bb4222508554ebe762f00395e2b
Eirik S. Morland <eirik@morland.no>
2024-05-04 19:38:37 +0200

    Also make sure the licence data ends up correct

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -14,12 +14,15 @@ class GeneratorTest extends TestCase
     {
         $new_private_key = bin2hex('real super secret yeah');
         $creator = new LicenceGenerator($new_private_key);
-        $licence = new Licence(time() + 3600);
+        $expriy = time() + 3600;
+        $licence = new Licence($expriy, ['some' => 'data']);
         $licence_key = $creator->generateLicenceKey($licence);
         // Let's also check its validity, yeah?
         $ec = new EdDSA('ed25519');
         $key = $ec->keyFromSecret($new_private_key);
         $checker = LicenceChecker::createFromLicenceAndKey($licence_key, $key->getPublic('hex'));
         self::assertTrue($checker->isValid());
+        self::assertEquals($expriy, $checker->getPayload()->getExpiry());
+        self::assertEquals(['some' => 'data'], $checker->getPayload()->getData());
     }
 }
